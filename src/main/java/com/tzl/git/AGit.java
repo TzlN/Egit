ab0563eaca6304f30c35d237647121f21ec185f7
Tu ZL <18721394619@163.com>
2016-09-01 22:55:39 +0800

    测试冲突

--- src/main/java/com/tzl/git/AGit.java
+++ src/main/java/com/tzl/git/AGit.java
@@ -3,8 +3,8 @@ package com.tzl.git;
 public class AGit {
 	public static void main(String[] args) {
 		int i = 11;
-		int b = 1;
-		int c = i/b;
+		int d = 1;
+		int c = i/d;
 		System.out.println(c);
 	}
 }
